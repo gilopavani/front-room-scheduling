347b71c7748ee94b1d4041975ec033c8dc99fead
gilopavani <gilopavani@gmail.com>
2025-07-01 21:41:03 -0300

    feat: atualizar o middleware para incluir novos caminhos de correspondência

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,12 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/booking/:path*", "/dashboard/:path*", "/(auth)/:path*"],
+  matcher: [
+    "/booking/:path*",
+    "/(auth)/:path*",
+    "/clients/:path*",
+    "/rooms/:path*",
+    "/logs/:path*",
+    "/profile/:path*",
+  ],
 };
